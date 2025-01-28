45c41ae0b83a3f7ed0d523beaf92325fa540b184
Norberto Padin <npadin@gmail.com>
2025-01-28 10:07:14 -0300

    updated file

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ if __name__ == "__main__":
     device = {
         "host": "10.1.100.1",
         "port": 830,
-        "username": "admin",
-        "password": "BvsTv3965!",
+        "username": "netconf",
+        "password": "netconf",
         "hostkey_verify": False,
     }
 
-    # try:
-    #     with manager.connect(**device) as nconf:
-    #         print("[green]Connected successfully![/green]")
-    #         print("[cyan]Server Capabilities:[/cyan]")
-    #         print(list(nconf.server_capabilities))
-    # except Exception as e:
-    #     print(f"[red]Error: {e}[/red]")
+    try:
+        nconf = manager.connect(**device, timeout=60)
+        if nconf:
+            print("[green]Connected successfully![/green]")
+            print("[cyan]Server Capabilities:[/cyan]")
+            print(list(nconf.server_capabilities))
+    except Exception as e:
+        print(f"[red]Error: {e}[/red]")
 
     # try:
     #     with manager.connect(**device) as nconf:
@@ -36,17 +37,17 @@ if __name__ == "__main__":
     # except Exception as e:
     #     print(f"[red]Error: {e}[/red]")
 
-    try:
-        nconf = manager.connect(**device, timeout=60)
-        if nconf:
-            print("[green]Connected successfully![/green]")
-            print("[cyan]Server Capabilities:[/cyan]")
-            nc_reply = nconf.get_config(source="running")
-            xml_data = etree.tostring(
-                nc_reply.data_ele, pretty_print=True).decode()
-            with open("running.xml", "wt") as f:
-                f.write(xml_data)
-        else:
-            print("[red]Failed to connect to the device.[/red]")
-    except Exception as e:
-        print(f"[red]Error: {e}[/red]")
+    # try:
+    #     nconf = manager.connect(**device, timeout=60)
+    #     if nconf:
+    #         print("[green]Connected successfully![/green]")
+    #         print("[cyan]Server Capabilities:[/cyan]")
+    #         nc_reply = nconf.get_config(source="running")
+    #         xml_data = etree.tostring(
+    #             nc_reply.data_ele, pretty_print=True).decode()
+    #         with open("running.xml", "wt") as f:
+    #             f.write(xml_data)
+    #     else:
+    #         print("[red]Failed to connect to the device.[/red]")
+    # except Exception as e:
+    #     print(f"[red]Error: {e}[/red]")
